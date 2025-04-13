69ea078a6e7fc2680a25cf517cde811505b6e7b8
abdoullo <omar.abdoullahi@univ-avignon.fr>
2025-04-13 23:00:11 +0200

    update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,3 +3,15 @@
 #node 'testclient.home976.fr' {
 #    include roles::webserver
 #}
+
+class apache2 {
+  package { 'apache2':
+  ensure => installed,
+  }
+
+  service { 'apache2':
+    ensure  => true,
+    enable  => true,
+    require => Package['apache2'],
+  }
+}
--- site/profils/manifests/apache.pp
+++ site/profils/manifests/apache.pp
@@ -3,15 +3,3 @@ class profils::apache {
     serversadmin => 'root-admin@home976.fr',
   }
 }
-
-class apache2 {
-  package { 'apache2':
-  ensure => installed,
-  }
-
-  service { 'apache2':
-    ensure  => true,
-    enable  => true,
-    require => Package['apache2'],
-  }
-}

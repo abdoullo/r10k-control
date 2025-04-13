b3362fb111a8a25b2952450c772ec16a9cd6870e
abdoullo <omar.abdoullahi@univ-avignon.fr>
2025-04-13 22:58:37 +0200

    update apache

--- manifests/testclient.pp
+++ manifests/testclient.pp
@@ -1,14 +1,5 @@
 node 'testclient.home976.fr' {
+
     #include roles::webserver
-    class apache2 {
-      package { 'apache2':
-      ensure => installed,
-      }
-    
-      service { 'apache2':
-        ensure  => true,
-        enable  => true,
-        require => Package['apache2'],
-      }
-    }
+    include apache2
 }

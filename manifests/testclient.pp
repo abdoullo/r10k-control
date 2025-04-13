2a655694c956fc27454b03394171c7611dc71212
abdoullo <omar.abdoullahi@univ-avignon.fr>
2025-04-13 22:55:01 +0200

    install apache2 testclient

--- manifests/testclient.pp
+++ manifests/testclient.pp
@@ -1,4 +1,14 @@
 node 'testclient.home976.fr' {
     #include roles::webserver
-     class { 'apache': }
+    class apache2 {
+      package { 'apache2':
+      ensure => installed,
+      }
+    
+      service { 'apache2':
+        ensure  => true,
+        enable  => true,
+        require => Package['apache2'],
+      }
+    }
 }
